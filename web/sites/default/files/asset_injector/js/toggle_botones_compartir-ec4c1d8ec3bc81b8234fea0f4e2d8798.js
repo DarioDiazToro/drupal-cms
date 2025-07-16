430ab89aa228ffb57b66f57657e0d0c82c5f93f9
DarioDiazToro <haiverdiaztoro@gmail.com>
2025-07-16 16:18:10 -0500

    subiendo cambios actuales

--- web/sites/default/files/asset_injector/js/toggle_botones_compartir-ec4c1d8ec3bc81b8234fea0f4e2d8798.js
+++
@@ -1,9 +0,0 @@
-(function ($, Drupal) {
-  Drupal.behaviors.toggleShareButtons = {
-    attach: function (context, settings) {
-      $('.share-toggle', context).once('toggleShare').on('click', function () {
-        $(this).closest('.views-row').find('.share-icons').toggleClass('hidden');
-      });
-    }
-  };
-})(jQuery, Drupal);
